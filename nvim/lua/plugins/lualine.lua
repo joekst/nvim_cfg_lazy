d0aedd9f1659258641e78482c13180892258aefe
Jonas Ekstrand <joekst@gmail.com>
2023-02-12 21:31:53 +0100

    Added lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+        require("lualine").setup({
+            options = { theme = "gruvbox" },
+        })
+    end,
+
+}
